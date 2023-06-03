15fb1d10d07ab46a477fa39c0bc48214d5e93cb5
Jiaxi Zheng <jiaxihz@gmail.com>
2023-06-03 18:16:38 +0200

    server

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import dash
 from dash import html, dcc
 import dash_bootstrap_components as dbc
 from flask import Flask
+import os
 
 server = Flask(__name__)
 app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.FLATLY], server = server)
@@ -43,3 +44,6 @@ app.layout = html.Div(
         dash.page_container
     ]
 )
+
+if __name__ == "__main__":
+    app.run()

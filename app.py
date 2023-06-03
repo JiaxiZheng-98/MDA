a561d4879b9c47123755b8bb54cbed8844040bdb
Jiaxi Zheng <jiaxihz@gmail.com>
2023-06-03 18:00:32 +0200

    server

--- app.py
+++ app.py
@@ -5,34 +5,34 @@ from dash.dependencies import Input, Output, State
 import gunicorn
 
 app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.FLATLY])
+server = app.server
 
 navbar = dbc.NavbarSimple(
-     children=[
-         dbc.NavItem(
-             dbc.NavLink("Percentile Data", href="/Percentile")
-         ),
-         dbc.NavItem(dbc.DropdownMenu(
-             children=[
-                 dbc.DropdownMenuItem("General", href="/Event_general"),
-                 dbc.DropdownMenuItem("Monthly", href="/Event_monthly"),
-                 dbc.DropdownMenuItem("Weekday", href="/Event_weekday"),
-             ],
-             nav=True,
-             in_navbar=True,
-             label="Event Data",
-         )),
-         dbc.NavItem(dbc.NavLink("Level", href="/Level")),
-         dbc.NavItem(dbc.NavLink("Prediction", href="/Prediction")),
-     ],
-     brand="Home",
-     brand_href="/",
-     color="primary",
-     dark=True,
-     fluid=True,
-     links_left=True,
-     sticky='Top'
+    children=[
+        dbc.NavItem(
+            dbc.NavLink("Percentile Data", href="/Percentile")
+        ),
+        dbc.NavItem(dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("General", href="/Event_general"),
+                dbc.DropdownMenuItem("Monthly", href="/Event_monthly"),
+                dbc.DropdownMenuItem("Weekday", href="/Event_weekday"),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="Event Data",
+        )),
+        dbc.NavItem(dbc.NavLink("Level", href="/Level")),
+        dbc.NavItem(dbc.NavLink("Prediction", href="/Prediction")),
+    ],
+    brand="Home",
+    brand_href="/",
+    color="primary",
+    dark=True,
+    fluid=True,
+    links_left=True,
+    sticky='Top'
 )
-
 
 app.layout = html.Div(
     [

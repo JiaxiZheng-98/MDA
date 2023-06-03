d16fa16e28c8670a62cd2e2087664adbaf1d6aa5
Jiaxi Zheng <jiaxihz@gmail.com>
2023-06-03 18:31:25 +0200

    server

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 import dash
 from dash import html, dcc
 import dash_bootstrap_components as dbc
-from flask import Flask
-import os
 
-server = Flask(__name__)
-server.secret_key = os.environ.get('secret_key', 'secret')
+
 app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.FLATLY], server = server)
 server = app.server
-app.config.supress_callback_exceptions = True
 
 navbar = dbc.NavbarSimple(
     children=[
@@ -46,3 +42,6 @@ app.layout = html.Div(
         dash.page_container
     ]
 )
+
+if __name__ == "__main__":
+    app.run_server()
